35569a2a2ee97b79fa4bcea665b9b573114c56fc
loxt <emannuelloxt@hotmail.com>
2020-10-23 08:02:53 -0400

    chore: WIP setup initial

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from "react";
+
+export default function Home(props) {
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div>Next.js app starter</div>
+    </div>
+  );
+}
